fba21bb85125eee7d238ac226c33351f3fd390aa
Recep <0.recepyildirim@gmail.com>
2022-02-26 20:51:55 +0300

    Theme has been adjusted.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  createTheme,
+  fontWeight,
+  ThemeProvider,
+  typography,
+} from "@mui/system";
+import { purple } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    secondary: purple,
+  },
+
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLigth: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
 function App() {
-  return <div>Take a Note</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes></Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
